4768eb02c0fb72aa848f7df6c95c99c6c5a5e010
Mahendra Tennakoon <mahendra.tennakoon@syscolabs.com>
2019-01-24 11:31:15 +0530

    Create order button

--- src/components/OrderList.js
+++ src/components/OrderList.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Segment, Header, List } from 'semantic-ui-react';
 import Order from './Order';
-import { Message } from 'semantic-ui-react';
+import { Message, Button, Icon } from 'semantic-ui-react';
 
 const OrderList = (props) => {
     return (
@@ -9,7 +9,7 @@ const OrderList = (props) => {
             <Header as='h1' block color="blue">
                 Orders
             </Header>
-            <Segment>
+            <Segment attached>
                 {
                     props.error &&
                     <Message negative>
@@ -22,6 +22,12 @@ const OrderList = (props) => {
                         {props.orders.map((order) => <Order key={order.order_id} id={order.order_id} date={order.created_date} />)}
                     </List>
                 }
+            </Segment>
+            <Segment attached='bottom' clearing>
+                <Button secondary icon labelPosition='left' floated="right">
+                    <Icon name='cart plus' />
+                    Create Order
+                </Button>
             </Segment>
         </div>
     );
