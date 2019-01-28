c738a1575b109b09131134d0a97a230c6760691e
Mahendra Tennakoon <mahendrathennakoon@gmail.com>
2019-01-28 12:24:39 +0530

    Store details of items to add

--- src/components/AddItems.js
+++ src/components/AddItems.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
 import Item from './Item';
-import { Table, Button, Icon } from 'semantic-ui-react';
+import { Table, Button, Icon, Input } from 'semantic-ui-react';
 const axios = require('axios');
 
 class AddItems extends Component {
+    newItems = [];
     state = {
         items: [],
         error: undefined
     };
+    handleChangeQuantity = (item_id, quantity) => {
+        const itemIndex = this.newItems.findIndex(item => item.id === item_id);
+
+        let parseQty = parseInt(quantity, 10);
+
+        if (isNaN(parseQty)) {
+            parseQty = 0;
+        }
+
+        if (itemIndex === -1) {
+            this.newItems.push({id: item_id, quantity: parseQty});
+        } else {
+            this.newItems[itemIndex].quantity = parseQty;
+        }
+    }
+    handleSave
     calculateTotalPrice = () => {
         return this.state.items.reduce((accumulator, currentValue) => { return accumulator + currentValue.price }, 0);
     };
@@ -41,11 +58,21 @@ class AddItems extends Component {
                 </Table.Header>
                 <Table.Body>
                     {
-                        this.state.items.map((item) => <Item item={item} key={item.id} />)
+                        this.state.items.map((item) => (
+                            <Table.Row key={item.id}>
+                                <Table.Cell>{item.id}</Table.Cell>
+                                <Table.Cell>{item.name}</Table.Cell>
+                                <Table.Cell>{item.price}</Table.Cell>
+                                <Table.Cell>
+                                    <Input placeholder='0' onChange={(e) => {
+                                        this.handleChangeQuantity(item.id, e.target.value);
+                                    }} />
+                                </Table.Cell>
+                            </Table.Row>
+                        ))
                     }
                 </Table.Body>
                 <Table.Footer fullWidth>
-                    
                     <Table.Row>
                         <Table.HeaderCell colSpan='4'>
                             <Button floated='right' icon labelPosition='left' primary size='small'>
