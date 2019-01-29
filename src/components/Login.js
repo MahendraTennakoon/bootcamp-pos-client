c7d98165d9fd77c1021864975213fa26a2c7e700
Mahendra Tennakoon <mahendrathennakoon@gmail.com>
2019-01-29 15:19:00 +0530

    Login UI

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,36 @@
 import React, { Component } from 'react';
+import { Container, Segment, Header, Form, Button } from 'semantic-ui-react';
 
 class Login extends Component {
+    validateCredentials = (e) => {
+        const username = e.target.elements["username"].value;
+        const password = e.target.elements["password"].value;
+        
+        this.props.history.push('/orders');
+    }
     render() {
-        return (<p>Login</p>);
+        return (
+            <div>
+                <Container>
+                    <Header as='h2' attached='top'>
+                        Log In
+                    </Header>
+                    <Segment attached>
+                        <Form onSubmit={this.validateCredentials}>
+                            <Form.Field>
+                                <label>User name</label>
+                                <input name="username" placeholder='User name' />
+                            </Form.Field>
+                            <Form.Field>
+                                <label>Password</label>
+                                <input name="password" placeholder='Password' type="password" />
+                            </Form.Field>
+                            <Button type='submit'>Log In</Button>
+                        </Form>
+                    </Segment>
+                </Container>
+            </div>
+        );
     }
 }
 
