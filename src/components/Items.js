2d7a3e4f493085819981f4919c84b5a1ae6f3290
Mahendra Tennakoon <mahendrathennakoon@gmail.com>
2019-01-30 20:14:49 +0530

    DIsmiss add items modal after adding items.

--- src/components/Items.js
+++ src/components/Items.js
@@ -11,6 +11,7 @@ class Items extends Component {
     state = {
         items: [],
         error: undefined,
+        modalOpen: false
     };
     setItemQuantity = (item_id, quantity, edit_status) => {
         const index = this.state.items.findIndex(item => item.id === item_id);
@@ -22,7 +23,14 @@ class Items extends Component {
         this.setState((prevState) => ({
             items: [...prevState.items, ...items]
         }));
+        this.handleCloseModal();
     };
+    handleShowModal = () => {
+        this.setState(() => ({
+            modalOpen: true
+        }));
+    };
+    handleCloseModal = () => this.setState({ modalOpen: false });
     handleRemoveItem = (item_id) => {
         axios
             .delete(`http://localhost:8080/orders/${this.props.order_id}/${item_id}`)
@@ -79,76 +87,79 @@ class Items extends Component {
             <div>
                 {
                     this.props.server_error ?
-                    <Message negative>
-                        <Message.Header>{this.props.server_error}</Message.Header>
-                    </Message>:
-                <Table compact celled>
-                    <Table.Header>
-                        <Table.Row>
-                            <Table.HeaderCell>Item ID</Table.HeaderCell>
-                            <Table.HeaderCell>Name</Table.HeaderCell>
-                            <Table.HeaderCell>Price</Table.HeaderCell>
-                            <Table.HeaderCell>Quantity</Table.HeaderCell>
-                            <Table.HeaderCell></Table.HeaderCell>
-                        </Table.Row>
-                    </Table.Header>
-                    <Table.Body>
-                        {
-                            this.state.items.map((item) => <Item setItemQuantity={this.setItemQuantity} handleRemoveItem={this.handleRemoveItem} item={item} key={item.id} />)
-                        }
-                    </Table.Body>
+                        <Message negative>
+                            <Message.Header>{this.props.server_error}</Message.Header>
+                        </Message> :
+                        <Table compact celled>
+                            <Table.Header>
+                                <Table.Row>
+                                    <Table.HeaderCell>Item ID</Table.HeaderCell>
+                                    <Table.HeaderCell>Name</Table.HeaderCell>
+                                    <Table.HeaderCell>Price</Table.HeaderCell>
+                                    <Table.HeaderCell>Quantity</Table.HeaderCell>
+                                    <Table.HeaderCell></Table.HeaderCell>
+                                </Table.Row>
+                            </Table.Header>
+                            <Table.Body>
+                                {
+                                    this.state.items.map((item) => <Item setItemQuantity={this.setItemQuantity} handleRemoveItem={this.handleRemoveItem} item={item} key={item.id} />)
+                                }
+                            </Table.Body>
 
-                    <Table.Footer fullWidth>
-                        <Table.Row>
-                            <Table.HeaderCell />
-                            <Table.HeaderCell />
-                            <Table.HeaderCell>
-                                <strong>Total: {this.state.items.length > 0 && <span>{this.calculateTotalPrice()}</span>}</strong>
-                            </Table.HeaderCell>
-                            <Table.HeaderCell colSpan='3'>
-                                <strong>Total: {this.state.items.length > 0 && <span>{this.calculateTotalQuantity()}</span>}</strong>
-                            </Table.HeaderCell>
-                        </Table.Row>
-                        <Table.Row>
-                            <Table.HeaderCell colSpan='5'>
-                                <Modal trigger={
-                                    <Button floated='right' icon labelPosition='left' primary size='small'>
-                                        <Icon name='add' /> Add Items
-                                </Button>
-                                }>
-                                    <Modal.Header>Add Items</Modal.Header>
-                                    <Modal.Content>
-                                        <AddItems existingItems={this.state.items} handleAdd={this.handleAdd} />
-                                    </Modal.Content>
-                                </Modal>
-                            </Table.HeaderCell>
-                        </Table.Row>
-                        <Table.Row>
-                            <Table.HeaderCell colSpan='5'>
-                                <Button
-                                    floated='right'
-                                    icon
-                                    labelPosition='left'
-                                    negative
-                                    size='small'
-                                    onClick={this.handleDiscard}
-                                >
-                                    <Icon name='cancel' /> Discard
+                            <Table.Footer fullWidth>
+                                <Table.Row>
+                                    <Table.HeaderCell />
+                                    <Table.HeaderCell />
+                                    <Table.HeaderCell>
+                                        <strong>Total: {this.state.items.length > 0 && <span>{this.calculateTotalPrice()}</span>}</strong>
+                                    </Table.HeaderCell>
+                                    <Table.HeaderCell colSpan='3'>
+                                        <strong>Total: {this.state.items.length > 0 && <span>{this.calculateTotalQuantity()}</span>}</strong>
+                                    </Table.HeaderCell>
+                                </Table.Row>
+                                <Table.Row>
+                                    <Table.HeaderCell colSpan='5'>
+                                        <Modal
+                                            open={this.state.modalOpen}
+                                            onClose={this.handleCloseModal}
+                                            trigger={
+                                                <Button onClick={this.handleShowModal} floated='right' icon labelPosition='left' primary size='small'>
+                                                    <Icon name='add' /> Add Items
+                                    </Button>
+                                            }>
+                                            <Modal.Header>Add Items</Modal.Header>
+                                            <Modal.Content>
+                                                <AddItems existingItems={this.state.items} handleAdd={this.handleAdd} />
+                                            </Modal.Content>
+                                        </Modal>
+                                    </Table.HeaderCell>
+                                </Table.Row>
+                                <Table.Row>
+                                    <Table.HeaderCell colSpan='5'>
+                                        <Button
+                                            floated='right'
+                                            icon
+                                            labelPosition='left'
+                                            negative
+                                            size='small'
+                                            onClick={this.handleDiscard}
+                                        >
+                                            <Icon name='cancel' /> Discard
                             </Button>
-                                <Button
-                                    floated='right'
-                                    icon
-                                    labelPosition='left'
-                                    color='black'
-                                    size='small'
-                                    onClick={this.handleSave}
-                                >
-                                    <Icon name='save' /> Save
+                                        <Button
+                                            floated='right'
+                                            icon
+                                            labelPosition='left'
+                                            color='black'
+                                            size='small'
+                                            onClick={this.handleSave}
+                                        >
+                                            <Icon name='save' /> Save
                             </Button>
-                            </Table.HeaderCell>
-                        </Table.Row>
-                    </Table.Footer>
-                </Table>
+                                    </Table.HeaderCell>
+                                </Table.Row>
+                            </Table.Footer>
+                        </Table>
                 }
             </div>
         )
