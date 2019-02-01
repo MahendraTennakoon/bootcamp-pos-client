c07c47fd90139b961035af1af21a2ecdfa1b3b38
Mahendra Tennakoon <mahendrathennakoon@gmail.com>
2019-02-01 10:29:24 +0530

    Breadcrumb for navigating back to orders.

--- src/components/OrderDetails.js
+++ src/components/OrderDetails.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
-import { Segment, Header, Label } from 'semantic-ui-react';
+import { Segment, Header, Label, Breadcrumb } from 'semantic-ui-react';
 import Items from './Items';
+import { Link } from "react-router-dom";
+
 
 class OrderDetails extends Component {
     render() {
         return (
             <div>
                 <Header as='h1' block color="blue">
-                    Order Details
+                    <Breadcrumb size="massive">
+                        <Breadcrumb.Section link>
+                            <Link to={'/orders'}>Orders</Link>
+                        </Breadcrumb.Section>
+                        <Breadcrumb.Divider icon='right angle' />
+                        <Breadcrumb.Section link>Order Details</Breadcrumb.Section>
+                    </Breadcrumb>
                 </Header>
                 <Segment attached>
                     <Label attached='top'>ORDER ID: {this.props.match.params.order_id}</Label>
