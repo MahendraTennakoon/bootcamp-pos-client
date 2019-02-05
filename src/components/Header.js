f3af5a13bde461624dc810f2e605fba7984b9d18
Mahendra Tennakoon <mahendrathennakoon@gmail.com>
2019-02-05 10:26:51 +0530

    Log out functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { Button, Image } from 'semantic-ui-react';
+import { withRouter } from "react-router-dom";
 
 
 const Header = (props) => {
+    const handleLogOut = () => {
+        localStorage.setItem('isAuthenticated', 'false');
+        props.history.push('/login');
+    };
+
     return (
         <div className="header_bar">
             <h1>{props.title}</h1>
-            {props.subtitle && <h2>{props.subtitle}</h2>}
+            <Button floated="right" color='yellow' onClick={handleLogOut}>Log Out</Button>
         </div>
     );
 };
@@ -14,4 +21,4 @@ Header.defaultProps = {
     title: 'Point of Sales System'
 };
 
-export default Header;
+export default withRouter(Header);
